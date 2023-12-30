d1a9551c995078ba015a92a3db9fb8ecf6adf621
suren-tanwar <tanwarsuren7@gmail.com>
2023-12-31 00:23:30 +0530

    commit1

--- src/routes/otpRoutes.js
+++
@@ -1,9 +0,0 @@
-// routes/otpRoutes.js
-const express = require('express');
-const otpController = require('../controllers/otpController');
-
-const router = express.Router();
-
-router.get('/generate-otp/:userId', otpController.generateOTPData);
-
-module.exports = router;
